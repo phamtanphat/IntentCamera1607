e9363cb5d3e8aca6c6dc11b8ab98b13b03143245
thuviengithub0208 <khoaphp@yahoo.com>
2018-08-22 19:27:14 +0700

    "Camera"

--- app/src/main/java/com/ptp/phamtanphat/intentcamera1607/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/intentcamera1607/MainActivity.java
@@ -1,13 +1,48 @@
 package com.ptp.phamtanphat.intentcamera1607;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCamera,btnGallery;
+    int Request_Code_Camera = 123;
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnGallery = findViewById(R.id.buttonGallery);
+        img = findViewById(R.id.imageview);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,Request_Code_Camera);
+
+            }
+        });
+        Bundle bundle = new Bundle();
+        bundle.putString("Chuoi","ABC");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == Request_Code_Camera && resultCode == RESULT_OK && data!= null){
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
